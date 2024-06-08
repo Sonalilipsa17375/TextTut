9822dfdf97a1d6eafd1ba566f468a6c973693056
Sonalilipsa17375 <sonalipatra20.11.2003@gmail.com>
2024-06-08 11:16:54 +0530

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,35 @@
+import React,{useState} from 'react'
+
+const Main = () => {
+  const handleonchange = (event)=>{
+    setText(event.target.value);
+  }
+  
+  const handleupclick = ()=>{
+    let newText = text.toUpperCase();
+    setText(newText);
+  }
+  const [text,setText] = useState("");
+  
+
+  return (
+    <>
+    <h1>TextTut</h1>
+    
+    <div className='container'>
+    
+        <input type="text" value={text} onChange={handleonchange} placeholder='Enter Text Here' className='ip-field'/>
+    
+    </div>
+    <div>
+        <button onClick={handleupclick} className='up-but'>Change To UpperCase</button>
+    </div>
+    <div className='summary'>
+  
+     <p>Text Summary - <span>Word Count - {text.split(" ").length} and Number of Characters - {text.length}</span></p>
+    </div>
+    </>
+  )
+}
+
+export default Main
